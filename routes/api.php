aa9700f67dfcc78def8b678bc33f6b11d78cc177
oscrodsol <oscar43521@gmail.com>
2022-08-12 14:12:18 +0200

    feat: Added a route for the welcome page

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/', function(){
+    return 'Welcome!';
+});
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
