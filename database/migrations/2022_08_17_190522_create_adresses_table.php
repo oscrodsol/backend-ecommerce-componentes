ead8e0af93a5457c6150f51c1c4a8d008307259d
oscrodsol <oscar43521@gmail.com>
2022-08-18 00:34:31 +0200

    feat: Filled the adresses table file

--- database/migrations/2022_08_17_190522_create_adresses_table.php
+++ database/migrations/2022_08_17_190522_create_adresses_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('adresses', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('city', 191);
+            $table->string('state', 191);
+            $table->string('zip', 191);
+            $table->string('street', 191);
+            $table->string('phone', 191);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
