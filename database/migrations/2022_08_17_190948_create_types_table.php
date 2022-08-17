0c626441f1380ad23a8f1859d0f600d7c8124572
oscrodsol <oscar43521@gmail.com>
2022-08-17 21:12:59 +0200

    feat: Created roles, products, types, ratings, quantities and role_user tables

--- database/migrations/2022_08_17_190948_create_types_table.php
+++ database/migrations/2022_08_17_190948_create_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('rating', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('rating');
+        Schema::dropIfExists('types');
     }
 };
