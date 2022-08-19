be879463229e9dd93e78a5242a9de86960732ade
oscrodsol <oscar43521@gmail.com>
2022-08-19 13:51:52 +0200

    feat: Filled the quantities table

--- database/migrations/2022_08_17_191059_create_quantities_table.php
+++ database/migrations/2022_08_17_191059_create_quantities_table.php
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('quantities', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
             $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
