82a85bf173193f4c02132b899f9c2fd3d26f58ed
oscrodsol <oscar43521@gmail.com>
2022-08-19 20:33:52 +0200

    feat: Filled the Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
+
+    public function types()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
