47c1110e72ce538fb9d5b7b9836c88028485abb7
oscrodsol <oscar43521@gmail.com>
2022-09-16 01:34:56 +0200

    feat: Created observation and state_id in orders table, created price and quantity in order_product

--- database/migrations/2022_08_17_191059_create_order_product_table.php
+++ database/migrations/2022_08_17_191059_create_order_product_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('order_id');
             $table->unsignedBigInteger('product_id');
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity');
             $table->timestamps();
 
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
