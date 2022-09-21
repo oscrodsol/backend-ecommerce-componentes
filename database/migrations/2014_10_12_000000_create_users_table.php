0f69618839f7dbf61ced7632911c4a1887fe641d
oscrodsol <oscar43521@gmail.com>
2022-09-21 23:20:16 +0200

    fix: Added a default state when a user is created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,14 +16,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nick', 191)->unique();
-            $table->string('name', 191)->nullable();
-            $table->string('surname', 191)->nullable();
+            $table->string('name', 191)->nullable()->default('');
+            $table->string('surname', 191)->nullable()->default('');
             $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 191);
-            $table->string('image', 191)->nullable();
-            $table->date('birth_date')->nullable();
-            $table->string('phone', 191)->nullable();
+            $table->string('image', 191)->nullable()->default('');
+            $table->date('birth_date')->nullable()->default("1900-01-01");
+            $table->string('phone', 191)->nullable()->default('');
             $table->boolean('status')->default(true);
             $table->rememberToken()->nullable();
             $table->timestamps();
